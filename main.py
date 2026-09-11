8f44a1e627257d2a51550de72298f9d3faa3577f
Dimitris <61980746+SV9TOQ@users.noreply.github.com>
2026-09-12 01:23:32 +0300

    Added gen_pre
    Added the function gen_pre used to trigger radio's vox so then it can transmit the data wanted.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ T = 1.0 / baud
 num_samples = int(sample_rate * T)
 bit_freq = [400, 1200, 2000, 2800]
 
-pre_freq = 1000
+pre_freq = 300
 pre_duration = 300 # Time in ms
 
 # ===== Functions =====
@@ -25,6 +25,17 @@ def bits_to_text(bits):
     for i in range(0, len(bits), 8):
         chars.append(chr(int(bits[i:i+8], 2)))
     return ''.join(chars)
+
+# === Transmit Sequence ===
+def encode(bits):
+
+    return tx_signal
+
+# gen_pre is used to trigger the radio's vox similar to how it is used in APRS AFSK
+def gen_pre():
+    num_samples = int(sample_rate*pre_duration/1000)
+    t = np.arange(num_samples)/sample_rate
+    return np.sin(2*np.pi*pre_freq*t)
 
 
 if __name__ == '__main__':
